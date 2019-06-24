9d82bc6dba860078c4faa621b5d17078f15b25e4
Devon Marantz <DMarantz@talentpath.com>
2019-06-24 12:13:10 -0700

    Protect local files

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
+
 const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
+  // console.log('Yooooo');
+  // const devon = { name: 'devon', hair: 'black', fresh: true };
   res.send('Hey! It works!');
+  // res.json(devon);
 });
 
 module.exports = router;
