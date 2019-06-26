92333795bd004b8cd6628ae319e40c7bb559efb5
Devon Marantz <DMarantz@talentpath.com>
2019-06-26 11:43:35 -0700

    Add reverse name route

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,13 @@ const router = express.Router();
 router.get('/', (req, res) => {
   // console.log('Yooooo');
   // const devon = { name: 'devon', hair: 'black', fresh: true };
-  res.send('Hey! It works!');
-  // res.json(devon);
+  // res.send('Hey! It works!');
+  res.json(req.query);
+});
+
+router.get('/reverse/:name', (req, res) => {
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
 });
 
 module.exports = router;
