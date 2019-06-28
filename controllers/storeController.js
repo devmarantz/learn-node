47c848d7c2c15526602423ab7dd2c4a0ea92dfca
Devon Marantz <DMarantz@talentpath.com>
2019-06-28 14:43:33 -0700

    Handle POST request

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -24,3 +24,7 @@ exports.homePage = (req, res) => {
 exports.addStore = (req, res) => {
   res.render('editStore', { title: '🤙Add Store' });
 };
+
+exports.createStore = (req, res) => {
+  res.json(req.body);
+};
